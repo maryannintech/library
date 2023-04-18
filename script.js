e0f98836f4495088c2e02d6053c39c18a3b572ae
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-18 16:03:29 +0800

    use prettier

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        if (read === 'finish') {
-            return `The ${title} by ${author}, ${pages} pages, finished reading`;
-        }
-        else {
-            return `The ${title} by ${author}, ${pages} pages, not done reading`;
-        }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    if (read === "finish") {
+      return `The ${title} by ${author}, ${pages} pages, finished reading`;
+    } else {
+      return `The ${title} by ${author}, ${pages} pages, not done reading`;
     }
-    addBookToLibrary(title, author, pages, read, this.info);
+  };
+  addBookToLibrary(title, author, pages, read, this.info);
 }
 
 function addBookToLibrary(title, author, pages, read, info) {
-    myLibrary.push(title, author, pages, read, info);
+  myLibrary.push(title, author, pages, read, info);
 }
 
-const newBook = new Book('good omens', 'neil gaiman', '512', 'finish');
+const newBook = new Book("good omens", "neil gaiman", "512", "finish");
 console.log(newBook);
 console.log(myLibrary);
-
-
